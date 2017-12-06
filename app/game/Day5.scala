e1b590c100b433836f5b3e9acc294accd99323cd
Jan Nylund <jan.nylund@wartsila.com>
2017-12-06 21:28:01 +0200

    Day 5 part 2

--- app/game/Day5.scala
+++ app/game/Day5.scala
@@ -15,4 +15,25 @@ object Day5 {
       stepMaze(steps + 1, nextPos, nextMaze)
     }
   }
+
+  def solveMaze2(input: String): Int = {
+    val maze = input.split("\\s+").map(i => Integer.parseInt(i)).toList
+    stepMaze2(0, 0, maze)
+  }
+
+  def stepMaze2(steps: Int, pos: Int, maze: List[Int]): Int = {
+    if (pos < 0 || pos >= maze.size) {
+      steps
+    } else {
+      val offset = maze(pos)
+      val nextOffset = if (offset >= 3) {
+        offset - 1
+      } else {
+        offset + 1
+      }
+      val nextMaze = maze.updated(pos, nextOffset)
+      val nextPos = pos + offset
+      stepMaze2(steps + 1, nextPos, nextMaze)
+    }
+  }
 }
