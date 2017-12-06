cd3d7d231e0a4cca7115f7de57078fab13c2ddf7
Jan Nylund <jan.nylund@wartsila.com>
2017-12-06 14:01:13 +0200

    Day 1 part 2

--- app/service/Utils.scala
+++ app/service/Utils.scala
@@ -10,6 +10,25 @@ object Utils {
     (n :+ n.head).sliding(2).toList.map(l => if (l(0) == l(1)) l(0) else 0).sum
   }
 
+  /**
+    * Day 1 part 2
+    */
+  def sumIfHalfWayEqual(input: String) = {
+    val n = stringAsIntList(input)
+    val size = n.size
+    val offset = size / 2
+
+    val values = n.zipWithIndex.map {
+      case (value, index) =>
+        if (value == n((index + offset) % size)){
+          value
+        } else {
+          0
+        }
+    }
+    values.sum
+  }
+
   def stringAsIntList(str: String): List[Int] = {
     str.toList.map(c => Integer.parseInt(c.toString));
   }
